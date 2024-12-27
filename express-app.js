31a4ba176a8c28b73f018535f14dd826b4c45fc4
Omeir Mujtaba <omeirr13@gmail.com>
2024-12-28 00:47:43 +0500

    worked with cookies

--- express-app.js
+++ express-app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+
 
 module.exports = async (app) => {
     app.use(express.static('public'));//this is so we can serve static files via css to the browser
     app.use(express.json());//it takes any json data that comes along with request, parses into a javascript object so we can use it inside code, and attaches that object to request object so we can access with request handler
-    
+    app.use(cookieParser());
+
     app.set('view engine', 'ejs');
 
     //routes
@@ -13,4 +16,19 @@ module.exports = async (app) => {
 
     app.use('/auth', authRoutes);
 
+
+    //cookies
+    app.get('/set-cookies', (req, res) => {
+        // res.setHeader('Set-Cookie', 'newUser=true');
+        res.cookie('newUser', false);
+        res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, secure: true, httpOnly: true });    
+        res.send('<h1>you got the cookies</h1>');
+    })
+
+    app.get('/read-cookies', (req, res) => {
+        const cookies = req.cookies;
+        console.log(cookies);
+        console.log(cookies.newUser ? 'ok' : 'no');
+        res.json(cookies);
+    })
 };
